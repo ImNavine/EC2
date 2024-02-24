a3104bd2e5dbcb79e8bb4ed84c45696a53703ab4
ImNavine <kevinadine14@gmail.com>
2024-02-24 12:47:48 -0500

    segundo commit

---
+++ app/src/main/java/pe/edu/idat/ec2/FormularioActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.ec2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+    }
+}
